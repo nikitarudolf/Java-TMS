544bd02fdc5053fc5090da1e31a222d34e8a16af
Nikita Rudolf <nikitosr424@yandex.ru>
2025-10-06 19:00:34 +0300

    bmw 2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,51 @@
-
 public class Main {
+
+
+    static class Car {
+        int color;
+        double maxSpeed;
+        int mileage;
+
+        public int getColor() {
+            return color;
+        }
+
+        public double getMaxSpeed() {
+            return maxSpeed;
+        }
+
+        public int getMileage() {
+            return mileage;
+        }
+    }
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello world!");
         System.out.println("Second commit");
         int a = 5;
         int b = 10;
-        System.out.println(a*b);
+        System.out.println(a * b);
 
-        for (int i =0; i<a*b; i++) {
+        for (int i = 0; i < a * b; i++) {
             System.out.println(i);
-            System.out.println(a*i);
+            System.out.println(a * i);
         }
 
-        for (int i =0; i<5; i++) {
-            System.out.println(a^i);
-            System.out.println(a*b);
+        for (int i = 0; i < 5; i++) {
+            // Changed ^ to * - ^ is bitwise XOR, not exponentiation
+            System.out.println(a * i);
+            System.out.println(a * b);
         }
 
-        class Car {
-            int color;
-            double maxSpeed;
-            int mileage;
-
-            public int getColor;
-            public double getMaxSpeed;
-            public int getMileage;
-
-            public int getColor() {
-                return color;
-            }
-
-            public double getMaxSpeed() {
-                return maxSpeed;
-            }
-
-            public int getMileage() {
-                return mileage;
-            }
-        }
-
+        // Create Car instance
         Car bmw = new Car();
         bmw.color = 12245;
         bmw.maxSpeed = 243;
         bmw.mileage = 76543;
 
-
+        // Call the getter methods (with parentheses)
+        System.out.println(bmw.getColor());
+        System.out.println(bmw.getMaxSpeed());
+        System.out.println(bmw.getMileage());
     }
 }
