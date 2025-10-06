7d90f2e012340c67af6087bd45f291af7295da87
Nikita Rudolf <nikitosr424@yandex.ru>
2025-10-06 19:01:32 +0300

    corrected

--- src/Main.java
+++ src/Main.java
@@ -30,20 +30,17 @@ public class Main {
             System.out.println(i);
             System.out.println(a * i);
         }
-
         for (int i = 0; i < 5; i++) {
-            // Changed ^ to * - ^ is bitwise XOR, not exponentiation
+
             System.out.println(a * i);
             System.out.println(a * b);
         }
 
-        // Create Car instance
         Car bmw = new Car();
         bmw.color = 12245;
         bmw.maxSpeed = 243;
         bmw.mileage = 76543;
 
-        // Call the getter methods (with parentheses)
         System.out.println(bmw.getColor());
         System.out.println(bmw.getMaxSpeed());
         System.out.println(bmw.getMileage());
