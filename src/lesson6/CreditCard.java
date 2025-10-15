01924c67b6d12fb88f0dba7fb2aa61cd2fadb17e
Nikita Rudolf <nikitosr423@gmail.com>
2025-10-15 21:41:39 +0300

    Homework lesson 6

---
+++ src/lesson6/CreditCard.java
@@ -0,0 +1,30 @@
+package lesson6;
+
+class CreditCard {
+
+    private final int accountNumber;
+    private int creditsAmount;
+
+    public CreditCard(int number, int initialAmount) {
+        this.accountNumber = number;
+        this.creditsAmount = initialAmount;
+    }
+
+    public void addCredits(int a) {
+        this.creditsAmount += a;
+    }
+
+    public void withdrawCredits(int a) {
+
+        if(this.creditsAmount < a) {
+            System.out.println("Недостаточно средств");
+            return;
+        }
+        this.creditsAmount -= a;
+    }
+
+    public void outputInfo() {
+        System.out.println("Номер счета: " + this.accountNumber + "\n" + "Баланс: " + this.creditsAmount);
+    }
+
+}
--- src/lesson6/LessonHomework.java
+++ src/lesson6/LessonHomework.java
@@ -15,36 +15,6 @@ public class LessonHomework {
 //                Тестовый сценарий для проверки: Положите деньги на первые две карточки и снимите с
 //        третьей. Выведите на экран текущее состояние всех трех карточек
 
-        class CreditCard {
-
-            private int accountNumber;
-            private int creditsAmount;
-
-            public CreditCard(int number, int initialAmount) {
-                this.accountNumber = number;
-                this.creditsAmount = initialAmount;
-            }
-
-            public void addCredits(int a) {
-                this.creditsAmount += a;
-            }
-
-            public void withdrawCredits(int a) {
-
-                if(this.creditsAmount < a) {
-                    System.out.println("Недостаточно средств");
-                    return;
-                }
-                this.creditsAmount -= a;
-            }
-
-            public void outputInfo() {
-                System.out.println("Номер счета: " + this.accountNumber + "\n" + "Баланс: " + this.creditsAmount);
-            }
-
-        }
-
-
         CreditCard visa = new CreditCard(0115102025, 500);
         CreditCard mastercard = new CreditCard(0215102025, 1500);
         CreditCard unionpay = new CreditCard(0315102025, 2500);
