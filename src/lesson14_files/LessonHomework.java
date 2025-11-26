e9674b325f6ddffa55759a70e594bbce2a2f7ba1
Nikita Rudolf <nikitosr423@gmail.com>
2025-11-26 19:00:30 +0300

    HW 14 FILES

--- src/lesson14_files/LessonHomework.java
+++ src/lesson14_files/LessonHomework.java
@@ -1,4 +1,36 @@
 package lesson14_files;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class LessonHomework {
+    public static void main(String[] args) {
+
+        try(FileReader fr = new FileReader("C:\\Users\\nikit\\IdeaProjects\\01102025\\src\\lesson14_files\\text.txt")) {
+            int index;
+            String longestWord = "";
+            String tempWord = "";
+            while((index = fr.read()) != -1) {
+                if( (index >= 65 && index <= 90 ) || (index >= 97 && index <= 122 )) {
+                    tempWord += ((char) index);
+                } else {
+                    if(tempWord.length() > longestWord.length() ) {
+                        longestWord = tempWord;
+                    }
+                    tempWord = "";
+                }
+            }
+
+            System.out.println(longestWord);
+            FileWriter fw = new FileWriter("C:\\Users\\nikit\\IdeaProjects\\01102025\\src\\lesson14_files\\result.txt");
+            fw.write(longestWord);
+            fw.flush();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
